ccd7abb1be6510e10e8da8463c5dc95b3975edf9
Yuki Oshima <oshima.y.0613@gmail.com>
2019-03-22 23:21:33 +0900

    Add Spring Boot initial project

---
+++ src/main/java/springdockerexample/SpringDockerExampleApplication.java
@@ -0,0 +1,13 @@
+package springdockerexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDockerExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDockerExampleApplication.class, args);
+	}
+
+}
